46f30ebab522f9e6b1d8078330aba43949261349
Evan Shortiss <evanshortiss@gmail.com>
2020-03-23 17:32:02 -0700

    fix: remove yarn and setup fix env errors

--- solutions/traffic-application-frontend/server.js
+++ solutions/traffic-application-frontend/server.js
@@ -6,10 +6,14 @@ const env = require('env-var')
 const { resolve } = require('path')
 
 const PORT = env.get('PORT').default(8080).asPortNumber()
+const HOST = env.get('HOST').default('0.0.0.0').asString()
 
 app.use(require('morgan')('combined'))
 app.use(express.static(resolve(__dirname, 'dist/')))
 
-app.listen(PORT, (err) => {
+app.listen(PORT, HOST, (err) => {
+  if (err) {
+    throw err
+  }
   console.log(new Date().toJSON(),`server started listening on ${PORT}\n`)
 })
