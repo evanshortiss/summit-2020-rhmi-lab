751ecfc1828c62d2071b02827652d2b1c8b985fe
Jameel Briones <JameelB@users.noreply.github.com>
2020-04-20 18:32:17 +0100

    add scripts for installing per user fuse online (#4)

---
+++ rhmi/user-fuse-cleanup.sh
@@ -0,0 +1,23 @@
+DEV_USERNAME="${DEV_USERNAME:-evals}"
+
+
+# Remove all syndesis custom resource
+for i in $(oc get syndesis --all-namespaces | grep $DEV_USERNAME | awk '{print $2}'); do
+    oc delete syndesis $i -n $i-fuse
+done
+
+# Remove subscriptions
+for i in $(oc get subscriptions --all-namespaces | grep $DEV_USERNAME | awk '{print $2}'); do
+    NAMESPACE=$(echo $i | sed 's/syndesis/fuse/g')
+    oc delete subscription $i -n $NAMESPACE
+done
+
+# Remove operator group
+for i in $(oc get operatorgroups --all-namespaces  | grep -v NAMESPACE | grep $DEV_USERNAME | awk '{print $1}'); do
+    oc delete operatorgroup $i-og -n $i
+done
+
+# Remove namespaces
+for i in $(oc get projects -l user-fuse-online=true | grep -v NAME | awk '{print $1}'); do
+    oc delete namespace $i
+done
